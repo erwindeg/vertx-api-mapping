1d0232dfc2e66e9fd9288114ad4a9cda283d3f6d
erwindeg <erwin@edegier.nl>
2016-09-28 16:23:06 +0200

    added aggregate of rest clients

--- src/main/java/nl/edegier/MainVerticle.java
+++ src/main/java/nl/edegier/MainVerticle.java
@@ -9,7 +9,28 @@ public class MainVerticle {
 
     public static void main(String[] args) {
         Vertx vertx = Vertx.vertx();
-        vertx.deployVerticle("js/server.js");
+       vertx.deployVerticle("js/server.js");
+
+
+//        HttpClient client = vertx.createHttpClient();
+//        Future f1 = Future.future();
+//        Future f2 = Future.future();
+//        client.getNow(80, "jsonplaceholder.typicode.com", "/posts/1", result ->{
+//            System.out.println(result.statusCode());
+//            result.bodyHandler(body -> f1.complete(body));
+//        });
+//
+//        client.getNow(80, "jsonplaceholder.typicode.com", "/posts/2", result ->{
+//            System.out.println(result.statusCode());
+//            result.bodyHandler(body -> f2.complete(body));
+//        });
+//        CompositeFuture.all(f1, f2).setHandler(ar -> {
+//            if (ar.succeeded()) {
+//                ar.result().list().forEach(System.out::println);
+//            } else {
+//                // At least one server failed
+//            }
+//        });
     }
 
 }
--- src/main/resources/js/subrouter1.js
+++ src/main/resources/js/subrouter1.js
@@ -1,11 +1,38 @@
 var Router = require("vertx-web-js/router");
+var Future = require("vertx-js/future");
+var CompositeFuture = require("vertx-js/composite_future");
 
 var restAPI = Router.router(vertx);
 
 restAPI.get("/hello").handler(function (rc) {
 
-  // TODO Handle the lookup of the product....
- rc.response().end("hello from subrouter");
+ //http://jsonplaceholder.typicode.com/posts/1
+
+ var call1Future = Future.future();
+ var call2Future = Future.future();
+ var client = vertx.createHttpClient();
+ client.getNow(80, "jsonplaceholder.typicode.com", "/posts/1", function (response) {
+    response.bodyHandler(function(body) {
+       call1Future.complete(body.toString());
+     })
+ });
+
+ client.getNow(80, "jsonplaceholder.typicode.com", "/posts/2", function (response) {
+    response.bodyHandler(function(body) {
+          call2Future.complete(body.toString());
+        })
+  });
+
+  CompositeFuture.all(call1Future,call2Future).setHandler(function(futures){
+        var posts = [];
+        posts.push(JSON.parse(futures.result(0)));
+        posts.push(JSON.parse(futures.result(1)));
+        rc.response().putHeader("content-type", "application/json");
+        rc.response().setChunked(true);
+        rc.response().write(JSON.stringify(posts)).end();
+  });
+
+
 
 });
 
